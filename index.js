563b31f9ade97d787074b03d3e4616f69c3dd8c1
Eli Flanagan <efx@users.noreply.github.com>
2020-01-17 16:27:04 -0500

    work around yarn install

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// TODO find better spot for install
+const { execSync } = require('child_process');
+
+execSync('yarn');
 // eslint-disable-next-line
 require = require('esm')(module);
 module.exports = require('./src/action.js');
