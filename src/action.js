2224ddd5b95fe0ebf439da63b1808f11db8ce4bd
Eli Flanagan <efx@users.noreply.github.com>
2020-01-17 15:23:39 -0500

    fix execution

--- src/action.js
+++ src/action.js
@@ -2,17 +2,14 @@ import { convertConfig } from './helpers';
 import analyze from './analyze';
 import { getInput, setFailed } from '@actions/core';
 
-const configProperties = [
+export default async function main() {
+  try {
+    await analyze(convertConfig([
   'use-yarn',
   'repo-token',
-];
-
-async function main() {
-  try {
-    await analyze(convertConfig(config));
+]));
   } catch (e) {
     setFailed(e.message);
   }
 }
 
-export default main;
